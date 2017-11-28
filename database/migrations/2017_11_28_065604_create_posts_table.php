254c0cc24049501b97c3525d5509127bfe77db61
LeoHuang <leohuang1996@gmail.com>
2017-11-28 18:58:48 +0800

    (3-1)新增migration裡 up方法的內容

--- database/migrations/2017_11_28_065604_create_posts_table.php
+++ database/migrations/2017_11_28_065604_create_posts_table.php
@@ -15,6 +15,8 @@
      {
          Schema::create('posts', function (Blueprint $table) {
              $table->increments('id');
+             $table->string('title');
+             $table->text('content');
              $table->timestamps();
          });
      }

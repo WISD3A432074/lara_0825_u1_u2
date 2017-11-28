3baaf406b459ed68e38a3fd82a04da47d2072533
LeoHuang <leohuang1996@gmail.com>
2017-11-28 21:46:25 +0800

    Ch2練習4-2:讓資料有時間序列性、隨機感

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
  use Illuminate\Database\Seeder;
  use App\Post;
+ use Carbon\Carbon;
  class PostsTableSeeder extends Seeder
  {
      /**
@@ -11,11 +12,15 @@
      public function run()
      {
        Post::truncate();
-       
+
             foreach(range(1,20) as $number) {
+            $total=20;
             Post::create([
                 'title' => 'title ' . $number,
                 'content' => 'content ' . $number,
+                'is_feature' => rand(0,1),
+                'created_at' => Carbon::now() ->subDays($total - $number),
+                'updated_at' => Carbon::now() ->subDays($total - $number),
             ]);
         }
      }

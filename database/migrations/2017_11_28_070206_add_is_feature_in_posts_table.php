8528de10378fe86e5c0cdfaa0ad490ddf8cc10cb
LeoHuang <leohuang1996@gmail.com>
2017-11-28 19:07:03 +0800

    Revert "(7-2)運用 change(), after()指令來調整資料表結構"
    This reverts commit 1e419cefd2e54e2fdeb7399e132ad156a7476037.

--- database/migrations/2017_11_28_070206_add_is_feature_in_posts_table.php
+++ database/migrations/2017_11_28_070206_add_is_feature_in_posts_table.php
@@ -14,8 +14,7 @@
      public function up()
      {
          Schema::table('posts', function (Blueprint $table) {
-             $table ‐>string('title',500) ‐>change();
-             $table ‐>boolean('is_feature') ‐>default(false) ‐>after('content');
+             //
          });
      }
 

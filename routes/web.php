0385af28ac568b2d3940fad7563f54f7c35c89e2
LeoHuang <leohuang1996@gmail.com>
2017-11-28 19:22:08 +0800

    Ch1練習2-5:改用new方法新增資料

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
-\App\Post::create([
-     'title'=>'test title',
-     'content'=>'test content',
- ]);
+    $post = new \App\Post();
+    $post -> title = 'test title';
+    $post -> content ='test content';
+    $post -> save();
 });
